4058c4fca7742dd7ab772afae19e6ab31bd00b9e
Stanislav <stack@uni-dubna.ru>
2023-05-22 00:02:18 +0300

    map modal

--- src/app/map/map-page/map-page.component.ts
+++ src/app/map/map-page/map-page.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/Services/ProfileService';
+
+
+interface MapItem{
+  title: string;
+  type: string;
+  score: number;
+  image: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-map-page',
@@ -7,8 +17,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPageComponent  implements OnInit {
 
-  constructor() { }
+  city: string = "";
+  globalSearchString: string;
+  places: string[];
+  typesPlace: string[];
+  
+  open: boolean = false;
+  showItem!: MapItem;
+    
+  constructor(private profileService: ProfileService){
+    this.globalSearchString = '';
+    this.places = ["Додо Пицца", "Вкусно и точка"];
+    this.typesPlace = ["Кафе", "Ресторан", "Парк", "Кофейня", "Библиотека", "Коворкинг", "Анти-кафе", "Творческие пространства"];
+  }
+  
+  ngOnInit(){
+    this.city = this.profileService.getProfileById(this.profileService.getCurrentUserId())!.locale;
+  }
+  
+  searchbarInput(value: string){
+    this.globalSearchString = value;
+    if(this.globalSearchString){
+    // this.places = this.places.find(value)
+    }
+  }
 
-  ngOnInit() {}
+  openModal(){
+    
+    this.open = true;
+  }
+
+  closeModal(){
+    this.open = false;
+  }
 
 }
